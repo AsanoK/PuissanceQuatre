6d412e9cc0d8bb8bd46d45453008db69b840bdb2
Hugin <Hugin@172.18.23.45>
2017-04-05 17:11:45 +0200

    mise à jour IHM 05/04

--- src/IHM/Fenetre.java
+++ src/IHM/Fenetre.java
@@ -1,12 +1,41 @@
 package IHM;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
+import javax.swing.JFrame;
+/**
+ * classe représentant la fenêtre dans son ensemble : le plateau de jeu, mais aussi tous ce qui est autour
+ * @author Hugin
+ *
+ */
 public class Fenetre extends JFrame {
 
-	/**
+	/** 
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JPlateau plateau;
+	private static int XPREF ;
+	private static int YPREF ;
 	
+	/**
+	 * constructeur de la classe fenetre
+	 */
+	public Fenetre(){
+		initComponent();
+		this.pack();
+		this.setVisible(true);
+	}
+	/**
+	 * initialisation des composants graphiques de la fenêtre
+	 */
+	private void initComponent(){
+		plateau = new JPlateau(this);
+		this.setLayout(new BorderLayout());
+		this.add(plateau, BorderLayout.CENTER);
+		
+	}
+	public JPlateau getPlateau() {
+		return plateau;
+	}
 }
---
+++ src/IHM/JPlateau.java
@@ -0,0 +1,57 @@
+package IHM;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+import puissance4.Plateau;
+/**
+ * classe représentant le plateau de jeu et les cases qui le compose
+ * @author Hugin
+ *
+ */
+public class JPlateau extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private Plateau plateau;
+	private Fenetre fenetre;
+	private static int XPREF= 700;
+	private static int YPREF = 700;
+	private static Color BACKGROUNDCOLOR = Color.BLUE;
+	
+	/** 
+	 * constructeur de la classe
+	 * on fixe la taille par défaut de l'affichage
+	 */
+	public JPlateau(Fenetre fen){
+		fenetre = fen;
+		this.setPreferredSize(new Dimension(XPREF,YPREF));
+	}
+
+	/**
+	 * Méthode de "peinture" des différents éléments du JPlateau
+	 * On y met le fond, mais aussi les cases qui composent le plateau
+	 */
+	public void paintComponent(Graphics g){
+		g.setColor(BACKGROUNDCOLOR);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		for (int i = 0;i<7;i++){
+			for (int j = 0;j<6;j++){
+				paintCase(g,i,j);
+			}
+		}
+	}
+	/**
+	 * méthode permettant la peinture d'une case précise du plateau
+	 * @param g : le graphics utilisé pour peindre
+	 * @param i : l'indice en x de la case (par rapport au plateau, pas en pixel)
+	 * @param j: l'indice en y de la case (par rapport au plateau, pas en pixel)
+	 */
+	public void paintCase(Graphics g, int i, int j){
+		int pasx =(int) this.getSize().getWidth()/7;
+		int pasy =(int) this.getSize().getHeight()/7;
+		g.setColor(plateau.getCase(i, j).getCouleur());
+		g.fillOval(i*(pasx-1), j*(pasy-1), pasx, pasy);
+	}
+}
